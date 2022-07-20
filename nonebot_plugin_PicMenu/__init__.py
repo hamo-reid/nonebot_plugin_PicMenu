946aa74feb3aa86c6a4616601f1a003cff32b582
hamo-reid <61297321+hamo-reid@users.noreply.github.com>
2022-07-20 22:38:33 +0800

    change: 修改插件metadata

--- nonebot_plugin_PicMenu/__init__.py
+++ nonebot_plugin_PicMenu/__init__.py
@@ -9,20 +9,20 @@ from nonebot.adapters.onebot.v11.message import MessageSegment
 import re
 
 __plugin_meta__ = PluginMetadata(
-    name='nonebot_plugin_menu',
-    description='为插件提供可视化的帮助菜单',
+    name='PicMenu',
+    description='为已安装的插件提供可视化的帮助菜单',
     usage='显示所有插件及描述：菜单\n'
           '显示某一插件的功能菜单：菜单 插件名/序号\n'
-          '显示某一功能详情：菜单 插件名\序号 功能名\序号\n'
-          '注：插件名\功能名 支持模糊匹配',
+          '显示某一功能详情：菜单 插件名/序号 功能名/序号\n'
+          '注：插件名/功能名 支持模糊匹配',
     extra={
         'author': 'hamo-reid',
         'menu_data': [
             {
                 'func': '查询菜单',
-                'trigger_method': 'on_startwith',
-                'trigger_condition': '菜单',
-                'brief_des': '用于查询菜单的命令',
+                'trigger_method': '命令：菜单',
+                'trigger_condition': '开头匹配[Any]',
+                'brief_des': '用于查询本插件的各级菜单',
                 'detail_des': '查看插件总表、插件命令和命令详情,具体方法如下：\n'
                               '查看菜单总表：菜单\n'
                               '查看插件命令：菜单 插件名/编号\n'
