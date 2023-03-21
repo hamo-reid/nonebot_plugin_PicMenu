3f4a91f8c47ebf54b51a63783374e0054c26e762
student_2333 <lgc2333@126.com>
2023-03-21 21:14:34 +0800

    格式化

--- nonebot_plugin_PicMenu/__init__.py
+++ nonebot_plugin_PicMenu/__init__.py
@@ -15,29 +15,36 @@ from .metadata import __plugin_meta__
 
 
 driver = get_driver()
+
+
 @driver.on_bot_connect
 async def _():
     if not menu_manager.data_manager.plugin_menu_data_list:
         menu_manager.load_plugin_info()
 
+
 menu_manager = MenuManager()
-menu = on_startswith('菜单', priority=5)
-switch = on_fullmatch('开关菜单', permission=SUPERUSER | GROUP_ADMIN, priority=5)
+menu = on_startswith("菜单", priority=5)
+switch = on_fullmatch("开关菜单", permission=SUPERUSER | GROUP_ADMIN, priority=5)
 
 
 menu_switch = True
+
+
 @switch.handle()
 async def _():
     global menu_switch
     menu_switch = not menu_switch
     if menu_switch:
-        await switch.finish(MessageSegment.text('菜单已开启'))
+        await switch.finish(MessageSegment.text("菜单已开启"))
     else:
-        await switch.finish(MessageSegment.text('菜单已关闭'))
+        await switch.finish(MessageSegment.text("菜单已关闭"))
+
 
 async def check_switch(matcher: Matcher):
     if not menu_switch:
         matcher.skip()
+
 
 @menu.handle()
 async def _(event: Event, check=Depends(check_switch)):
@@ -45,35 +52,35 @@ async def _(event: Event, check=Depends(check_switch)):
     cmd_prefix_re = "|".join([x for x in driver.config.command_start if x])
     prefix_re = f"({cmd_prefix_re})?(菜单|功能|帮助) ?"
 
-    if match_result := re.match(rf'^{prefix_re}(?P<name>.*?) (?P<cmd>.*?)$', msg):
+    if match_result := re.match(rf"^{prefix_re}(?P<name>.*?) (?P<cmd>.*?)$", msg):
         plugin_name = match_result.group("name").strip()
         cmd = match_result.group("cmd").strip()
         temp = menu_manager.generate_func_details_image(plugin_name, cmd)
         if isinstance(temp, str):
-            if temp == 'PluginIndexOutRange':
-                await menu.finish(MessageSegment.text('插件序号不存在'))
-            elif temp == 'CannotMatchPlugin':
-                await menu.finish(MessageSegment.text('插件名过于模糊或不存在'))
-            elif temp == 'PluginNoFuncData':
-                await menu.finish(MessageSegment.text('该插件无功能数据'))
-            elif temp == 'CommandIndexOutRange':
-                await menu.finish(MessageSegment.text('命令序号不存在'))
+            if temp == "PluginIndexOutRange":
+                await menu.finish(MessageSegment.text("插件序号不存在"))
+            elif temp == "CannotMatchPlugin":
+                await menu.finish(MessageSegment.text("插件名过于模糊或不存在"))
+            elif temp == "PluginNoFuncData":
+                await menu.finish(MessageSegment.text("该插件无功能数据"))
+            elif temp == "CommandIndexOutRange":
+                await menu.finish(MessageSegment.text("命令序号不存在"))
             else:
-                await menu.finish(MessageSegment.text('命令过于模糊或不存在'))
+                await menu.finish(MessageSegment.text("命令过于模糊或不存在"))
         else:
-            await menu.finish(MessageSegment.image('base64://' + img2b64(temp)))
+            await menu.finish(MessageSegment.image("base64://" + img2b64(temp)))
 
-    elif match_result := re.match(rf'^{prefix_re}(?P<name>.*)$', msg):
+    elif match_result := re.match(rf"^{prefix_re}(?P<name>.*)$", msg):
         plugin_name = match_result.group("name").strip()
         temp = menu_manager.generate_plugin_menu_image(plugin_name)
         if isinstance(temp, str):
-            if temp == 'PluginIndexOutRange':
-                await menu.finish(MessageSegment.text('插件序号不存在'))
+            if temp == "PluginIndexOutRange":
+                await menu.finish(MessageSegment.text("插件序号不存在"))
             else:
-                await menu.finish(MessageSegment.text('插件名过于模糊或不存在'))
+                await menu.finish(MessageSegment.text("插件名过于模糊或不存在"))
         else:
-            await menu.finish(MessageSegment.image('base64://' + img2b64(temp)))
+            await menu.finish(MessageSegment.image("base64://" + img2b64(temp)))
 
     else:
         img = menu_manager.generate_main_menu_image()
-        await menu.finish(MessageSegment.image('base64://' + img2b64(img)))
+        await menu.finish(MessageSegment.image("base64://" + img2b64(img)))
